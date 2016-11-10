3eb3a5683fc6d870cdc2f8be1ed04155e5a14ca6
tamilisettinareshreddy <tnreddy.in@gmail.com>
2016-11-10 15:51:03 +0530

    update the java file with new method "getDetails"
    getDetails method has been added newly..

--- Test.java
+++ Test.java
@@ -1,6 +1,18 @@
 public class Test{
+private static int no;
+private static String name;
 
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		String status = getDetails(10,"Navya");
+		System.out.println("The status of the "+name+"is :"+status);
+	}
+	public static String getDetails(int number1,String name1){
+		//condition takes place
+		if(no==number1 ||name.equals(name1)){
+		return "true";
+		}
+		else
+			return "false";
 	}
 }
