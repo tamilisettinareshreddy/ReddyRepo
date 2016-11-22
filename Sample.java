ac7db436e574aa73668e862e008b076c745b9283
Naresh Reddy Tamilisetti <1265928@01hw743964.tcs-mobility.com>
2016-11-22 15:27:54 +0530

    Commiting the changes done in Sample.java by Navya

--- Sample.java
+++ Sample.java
@@ -5,6 +5,7 @@ public class Sample{
    }
 
    public static void main(String args[]){
+	System.out.println("Navya:/Sample : main method |Entry");
 	Sample s = new Sample();
         int result = s.add(10,20);
         System.out.println("The result is : "+result);
